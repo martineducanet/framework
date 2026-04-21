eebca7b6a3f7f37b89199d6d883710bf42e095f1
martineducanet <martin.nagy@educanet.cz>
2026-04-21 10:27:07 +0200

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new PDO('sqlite:knihovna.db');
-$db->exec("CREATE TABLE IF NOT EXISTS knihy (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nazev TEXT,
-    autor TEXT,
-    strany INTEGER
-)");
-?>
