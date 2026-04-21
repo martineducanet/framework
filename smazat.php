277d4e986993f91e4af69ea8402041414c25da4c
martineducanet <martin.nagy@educanet.cz>
2026-04-21 10:27:26 +0200

    Delete smazat.php

--- smazat.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-if (isset($_GET['id'])) {
-    $stmt = $db->prepare("DELETE FROM knihy WHERE id = :id");
-    $stmt->bindValue(':id', $_GET['id']);
-    $stmt->execute();
-}
-header("Location: index.php");
-?>
